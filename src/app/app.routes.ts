fb0a353c25d0c5fc680b68cc8aab257b8219e4ea
Ahmad nawzadi <afawad200@gmail.com>
2024-12-25 20:26:01 +0100

    about page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServiceComponent } from './service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home', component : HomeComponent
+    },
+    {
+        path: 'about', component : AboutComponent
+    },
+    {
+        path: 'contact', component : ContactComponent
+    },
+    {
+        path: 'service', component : ServiceComponent
+    }
+ 
+];
